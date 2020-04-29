c7f09590b4b78151560222a2e8add377ff9eea19
Tokyogame03 <64463487+Tokyogame03@users.noreply.github.com>
2020-04-30 01:02:17 +0800

    Update 巫妖编译.cs

--- 巫妖编译.cs
+++ 巫妖编译.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Andriew凝聚态和Andriew方程的时变演算
-            //《几何学的故事》
+            //几何学的故事
         }
     }
 }
